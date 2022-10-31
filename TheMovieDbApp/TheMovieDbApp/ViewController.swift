2db3d1f83d53f50f55ae125fee4da5a8c4d784b1
tk4chenko <tkachhhenko@gmail.com>
2022-10-31 19:02:44 +0200

    Initial commit

---
+++ TheMovieDbApp/TheMovieDbApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDbApp
+//
+//  Created by Artem Tkachenko on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
